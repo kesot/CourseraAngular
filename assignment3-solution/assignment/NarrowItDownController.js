b91064c0d33605db2ea7a77d799d2c9203ea986d
konstantin <ershov.k@outlook.com>
2016-10-07 11:56:49 +0200

    assignment3
    nothing is found implemented

--- assignment3-solution/assignment/NarrowItDownController.js
+++ assignment3-solution/assignment/NarrowItDownController.js
@@ -4,11 +4,23 @@ var app;
     var NarrowItDownController = (function () {
         function NarrowItDownController(menuSearchService) {
             this.menuSearchService = menuSearchService;
+            this.searchTerm = "";
         }
         NarrowItDownController.prototype.searchForItem = function () {
             var _this = this;
+            this.foundItems = [];
+            this.showNotFoundMessage = false;
+            if (this.searchTerm === "") {
+                this.showNotFoundMessage = true;
+                return;
+            }
             var promise = this.menuSearchService.getMatchedMenuItems(this.searchTerm);
-            promise.then(function (result) { return _this.foundItems = result; });
+            promise.then(function (result) {
+                _this.foundItems = result;
+                if (_this.foundItems.length === 0) {
+                    _this.showNotFoundMessage = true;
+                }
+            });
         };
         NarrowItDownController.prototype.removeItem = function (index) {
             this.foundItems.splice(index, 1);
--- assignment3-solution/assignment/NarrowItDownController.ts
+++ assignment3-solution/assignment/NarrowItDownController.ts
@@ -5,13 +5,28 @@
         static $inject = ['MenuSearchService'];
 
         foundItems: Array<IMenuItem>;
-        searchTerm: string;
+        searchTerm = "";
+        showNotFoundMessage: boolean;
 
         constructor(private menuSearchService: MenuSearchService) { }
 
         public searchForItem(): void {
+            this.foundItems = [];
+            this.showNotFoundMessage = false;
+
+            if (this.searchTerm === "") {
+                this.showNotFoundMessage = true;
+                return;
+            }
+
             var promise = this.menuSearchService.getMatchedMenuItems(this.searchTerm);
-            promise.then(result => this.foundItems = result);
+            promise.then(result => {
+                this.foundItems = result;
+                if (this.foundItems.length === 0) {
+                    this.showNotFoundMessage = true;
+                }
+            });
+
         }
 
         public removeItem(index: number): void {
