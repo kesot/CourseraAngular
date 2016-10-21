6db4b377a2582fc98cddfe2e9c5f0677067a6ca3
konstantin <ershov.k@outlook.com>
2016-10-21 15:13:55 +0200

    *

--- assignment5-solution/assignment/Scripts/src/menuapp/RoutesConfig.js
+++ assignment5-solution/assignment/Scripts/src/menuapp/RoutesConfig.js
@@ -2,6 +2,31 @@ var RoutesConfig = (function () {
     function RoutesConfig($stateProvider, $urlRouterProvider) {
         this.$stateProvider = $stateProvider;
         this.$urlRouterProvider = $urlRouterProvider;
+        $urlRouterProvider.otherwise("/");
+        // Home page
+        $stateProvider
+            .state('home', {
+            url: '/',
+            templateUrl: 'ng-views/home.html'
+        })
+            .state('categories', {
+            url: '/categories',
+            templateUrl: 'ng-views/categories.html',
+            controller: 'MainShoppingListController as mainList',
+            resolve: {
+                items: ['ShoppingListService', function (ShoppingListService) {
+                        return ShoppingListService.getItems();
+                    }]
+            }
+        })
+            .state('mainList.itemDetail', {
+            // url: '/item-detail/{itemId}',
+            templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+            controller: 'ItemDetailController as itemDetail',
+            params: {
+                itemId: null
+            }
+        });
     }
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
     return RoutesConfig;
--- assignment5-solution/assignment/Scripts/src/menuapp/RoutesConfig.ts
+++ assignment5-solution/assignment/Scripts/src/menuapp/RoutesConfig.ts
@@ -4,6 +4,37 @@
     constructor(
         private $stateProvider: angular.ui.IStateProvider,
         private $urlRouterProvider: angular.ui.IUrlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/");
+
+        // Home page
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'ng-views/home.html'
+            })
+
+            // Premade list page
+            .state('categories', {
+                url: '/categories',
+                templateUrl: 'ng-views/categories.html',
+                controller: 'MainShoppingListController as mainList',
+                resolve: {
+                    items: ['ShoppingListService', function (ShoppingListService) {
+                        return ShoppingListService.getItems();
+                    }]
+                }
+            })
+
+            // Item detail
+            .state('mainList.itemDetail', {
+                // url: '/item-detail/{itemId}',
+                templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+                controller: 'ItemDetailController as itemDetail',
+                params: {
+                    itemId: null
+                }
+            });
     }
 
 
